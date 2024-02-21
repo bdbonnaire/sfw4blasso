b440eddf62570a9ff7b6009738f992728f43a0ff
Basile D. Bonnaire <bdbonnaires@gmail.com>
2024-02-21 16:06:29 +0100

    added plotting solution for recovered spikes

--- gaussian2d_nb.jl
+++ gaussian2d_nb.jl
@@ -30,7 +30,7 @@ md"# Gaussian 2D Kernel"
 begin
 	# Model constants
 	sigmax=.01;
-	sigmay=.01;
+	sigmay=.1;
 	Dpx=.05;
 	Dpy=.05;
 	px=range(0., 1.,step=Dpx);
@@ -57,7 +57,7 @@ begin
 	sigma=.01;
 	# Load operator Phi
 	op=blasso.setoperator(kernel,a0,x0,sigma*w0);
-	blasso.plotobservation(op)
+	#blasso.plotobservation(op)
 end
 
 # ╔═╡ 436b02fb-2b8b-4e66-93ca-e344ecd90df0
@@ -75,18 +75,27 @@ end
 #end
 
 # ╔═╡ 01ed0bc2-3c35-4d51-8d31-bb084b592879
+# ╠═╡ show_logs = false
 result=sfw.sfw4blasso(fobj,kernel,op,options); # Solve problem
 
 # ╔═╡ 3c8fb520-419c-4626-b42c-38c813385179
-#sfw.show_result(result, options)
+# ╠═╡ show_logs = false
+sfw.show_result(result, options)
 
 # ╔═╡ 9f87f847-e175-4029-8870-eeeba7b6cebd
-function plotSpikes(x0,a0,result)
-	rec_amps = result.u[1:length(x0)]
-	rec_diracs = result.u[length(x0)+1:end]
-	plot(rec_diracs, rec_amps, line=:stem, label="Recovered Spikes", ls=:solid, lw=2, color=:red, marker=:circle, markersize=4)
-	plot!(x0, a0, line=:stem, label="Original", ls=:dash, color=:black, marker=:circle, markersize=2)
+function plotSpikes2D(x0,a0,result, op)
+	nb_rec_spikes = length(result.u) ÷ 3
+	rec_amps = result.u[1:nb_rec_spikes]
+	rec_diracs = result.u[nb_rec_spikes+1:end]
+	rec_diracs = reshape(rec_diracs, (nb_rec_spikes,2))
+	x0 = stack(x0)
+	blasso.plotobservation(op)
+	scatter!(x0[1,:], x0[2,:], label="Original", color=RGBA(1.,1.,1,0.5), marker=:circle, markerstroke=false, markersize=10)
+	scatter!(rec_diracs[:,1], rec_diracs[:,2], label="Recovered Spikes", color=:red, marker=:circle, markerstrokecolor=false, markersize=4)
 end
+
+# ╔═╡ c468d81f-bfbc-4934-8518-58efcc551f72
+plotSpikes2D(x0,a0, result, op)
 
 # ╔═╡ Cell order:
 # ╠═c13a86de-cb38-11ee-3890-c93e2ad0f39a
@@ -95,8 +104,9 @@ end
 # ╟─5dcf6210-5e2d-4c74-854e-5617749d8b8c
 # ╠═21f334a4-ef50-4e84-82c6-1d75a485d6b5
 # ╠═b4aea33e-6012-44b5-90ee-960e476382bd
+# ╠═c468d81f-bfbc-4934-8518-58efcc551f72
 # ╠═436b02fb-2b8b-4e66-93ca-e344ecd90df0
 # ╠═67884e0d-db4a-4a6a-ace9-ec88efe65d14
 # ╠═01ed0bc2-3c35-4d51-8d31-bb084b592879
 # ╠═3c8fb520-419c-4626-b42c-38c813385179
-# ╠═9f87f847-e175-4029-8870-eeeba7b6cebd
+# ╟─9f87f847-e175-4029-8870-eeeba7b6cebd
