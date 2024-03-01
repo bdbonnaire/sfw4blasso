b9bd52a028124eec5c41e67d846d82f98a3655ae
Basile D. Bonnaire <bdbonnaires@gmail.com>
2024-03-01 09:09:59 +0100

    files transfo eta

--- spec.jl
+++ spec.jl
@@ -15,29 +15,29 @@ N = length(pt)
 pω=0.:N-1 |> collect;
 Dpω=1.;
 # Bounds of domain
-θmin = -1.7
-θmax = 1.7
+θmin = -π/2 * .9
+θmax = π/2 * .9
 # Option solver
 options=sfw.sfw_options();
 # Load kernel attributes
 kernel=blasso.setSpecKernel(pt,pω,Dpt,Dpω,σ, θmin, θmax);
 println(typeof(kernel))
-# println(kernel.grid)
+#println(kernel.grid)
 #
 # Initial measure
-a_0=[1., 5.];
-x_0=[[100., atan(-100)], [300., 0]]
+a_0=[1.];
+x_0=[[.1, pi/4]]
 # Noise
 #srand(1);
 w0=randn(N^2);
-sigma=.005;
+sigma=.000;
 #print(typeof(w0*sigma))
 # Load operator Phi
 op=blasso.setSpecOperator(kernel, a_0,x_0,sigma*w0);
-image = reshape(a_0[1] * op.phi(x_0[1]), (N,N))
-image .+= reshape(a_0[2] * op.phi(x_0[2]), (N,N))
+# image = reshape(a_0[1] * op.phi(x_0[1]), (N,N))
+# image .+= reshape(a_0[2] * op.phi(x_0[2]), (N,N))
 
-lambda=0.01;
+lambda=0.001;
 # Load objective function
 fobj=blasso.setfobj(op,lambda);
 println(typeof(fobj))
