def197f722ed016afc1fc3c87e53e9f057767e17
Basile D. Bonnaire <bdbonnaires@gmail.com>
2024-03-07 17:15:12 +0100

    finalized testing on spectrograms

--- spec_nb.jl
+++ spec_nb.jl
@@ -18,17 +18,22 @@ end;
 # ╔═╡ bb6f403c-0897-4903-be58-8cd320f83d17
 begin 
 	using Revise
+	using PyCall
 	using blasso, sfw, certificate, toolbox
 end
 
 # ╔═╡ 5dcf6210-5e2d-4c74-854e-5617749d8b8c
 md"# Spectrogram Kernel"
 
+# ╔═╡ 648eff19-26fa-4d90-8968-daaacda95974
+md"## Setting up the kernel"
+
 # ╔═╡ 21f334a4-ef50-4e84-82c6-1d75a485d6b5
 begin
 	# Model constants
-	σ=.01;
-	Dpt= 1. /512;
+	N=256
+	σ=.02;
+	Dpt= 1. /(N-1);
 	pt=range(0., 1.,step=Dpt) |> collect;
 	N = length(pt)
 	pω=0.:N-1 |> collect;
@@ -37,49 +42,47 @@ begin
 	θmin = -π/2 * .9
 	θmax = π/2 *.9
 	# Option solver
-	options=sfw.sfw_options();
+	options=sfw.sfw_options(max_mainIter=2);
 	# Load kernel attributes
 	kernel=blasso.setSpecKernel(pt,pω,Dpt,Dpω,σ, θmin, θmax);
 	println(typeof(kernel))
 end
 
+# ╔═╡ 35e2ddf8-08da-40c9-9ec8-f6dc1e6b8d1c
+pushfirst!(pyimport("sys")."path", "")
+
+# ╔═╡ 81bdf362-fc97-499a-bb20-addabfe586ce
+md"## Noiseless Case"
+
+# ╔═╡ 9c7084c4-e7cb-45db-a762-15b821ed7263
+begin
+	py"""
+	from py_lib.signals import lin_chirp
+	from py_lib.spectrogram import gauss_spectrogram
+	
+	import numpy as np
+	#
+	# Two linear chirps
+	freq = 200
+	freq_lin = 100
+	a = -80
+	N = 256
+	sig1,_ = lin_chirp(freq, N=N)
+	sig2, t = lin_chirp(freq_lin ,c=a, N=N)
+	sig = sig1+sig2
+	"""
+	x0 = [[py"freq/N", 0], [py"freq_lin/N", atan(py"a/N")]]
+	spec_harm_lin = py"gauss_spectrogram"(py"sig", σ)
+	heatmap(abs.(spec_harm_lin), title="Spectrogram");
+end
+
 # ╔═╡ d71103a1-8e24-48b5-b6cd-9e9cf7a734a3
 begin
-	# Initial measure
-	a0=[1.];
-	x0=[[.1, 0.]]
-	# Noise
-	#srand(1);
-	w0=randn(N^2);
-	sigma=.00;
-	#print(typeof(w0*sigma))
+	# Creating the spectrograms
 	# Load operator Phi
-	op=blasso.setSpecOperator(kernel, a0,x0,sigma*w0);
+	op=blasso.setSpecOperator(kernel, vec(spec_harm_lin));
 	println(typeof(op))
 end
-
-# ╔═╡ 3d43a392-db29-4f4c-ba69-3d17d4978155
-begin
-	local image = zeros(N,N)
-	for i in 1:length(a0)
-		image += reshape(a0[i] * op.phi(x0[i]), (N,N))
-	end
-	image .+= reshape(sigma.*w0, (N,N))
-	heatmap(image)
-	#blasso.plotobservation(op)
-end
-
-# ╔═╡ c468d81f-bfbc-4934-8518-58efcc551f72
-# ╠═╡ disabled = true
-#=╠═╡
-begin
-	ar, xr = blasso.decompAmpPos(result.u, d=2)
-	imageRes = reshape(ar[1] * op.phi(xr[1]), (N,N))
-	imageRes .+= reshape(ar[2] * op.phi(xr[2]), (N,N))
-	imageRes .+= reshape(sigma.*w0, (N,N))
-	heatmap(imageRes)
-end
-  ╠═╡ =#
 
 # ╔═╡ 436b02fb-2b8b-4e66-93ca-e344ecd90df0
 begin
@@ -88,38 +91,161 @@ begin
 	fobj=blasso.setfobj(op,lambda);
 end
 
-# ╔═╡ 67884e0d-db4a-4a6a-ace9-ec88efe65d14
-# ╠═╡ disabled = true
-#=╠═╡
-#begin
-#	etaV = certificate.computeEtaV(x0, sign.(a0), op)
-#	#tt = collect(range(-1,1, 500))
-#	#plot(tt, etaV(tt))
-#end
-  ╠═╡ =#
-
 # ╔═╡ 01ed0bc2-3c35-4d51-8d31-bb084b592879
 result=sfw.sfw4blasso(fobj,kernel,op,options) # Solve problem
 
 # ╔═╡ 3c8fb520-419c-4626-b42c-38c813385179
 begin
-	println(x0)
+	println("Original parameters = $x0")
 	sfw.show_result(result, options)
 end
 
-# ╔═╡ 959b2184-787b-465e-9d1c-178510d9ea21
-println(x_0)
+# ╔═╡ 0dec2a4e-dd48-4ad1-9f89-54d63877129b
+md"## Noisy Case"
 
-# ╔═╡ 9f87f847-e175-4029-8870-eeeba7b6cebd
-function plotSpikes2D(x0,a0,result, op)
-	nb_rec_spikes = length(result.u) ÷ 3
-	rec_amps = result.u[1:nb_rec_spikes]
-	rec_diracs = result.u[nb_rec_spikes+1:end]
-	rec_diracs = reshape(rec_diracs, (nb_rec_spikes,2))
-	x0 = stack(x0)
-	blasso.plotobservation(op)
-	scatter!(x0[1,:], x0[2,:], label="Original", color=RGBA(1.,1.,1,0.5), marker=:circle, markerstroke=false, markersize=10)
-	scatter!(rec_diracs[:,1], rec_diracs[:,2], label="Recovered Spikes", color=:red, marker=:circle, markerstrokecolor=false, markersize=4)
+# ╔═╡ 7dea5cc0-9a54-4b55-90a5-9b1d3fc1fe5f
+begin
+	py"""
+	from py_lib.signals import lin_chirp
+	from py_lib.spectrogram import gauss_spectrogram
+	
+	import numpy as np
+	#
+	# Two linear chirps
+	freq = 200
+	freq_lin = 100
+	a = -80
+	N = 256
+	sig1,_ = lin_chirp(freq, N=N)
+	sig2, t = lin_chirp(freq_lin ,c=a, N=N)
+	sig = sig1+sig2
+	
+	# adding noise
+	sigma_noise = 0.2
+	rng = np.random.default_rng()
+	noise = rng.normal(0, sigma_noise, N) + rng.normal(0, sigma_noise, N)*1j
+	sig_noisy = sig + noise
+	"""
+	# computing spectrogram
+	spec_harm_lin_noisy = py"gauss_spectrogram"(py"sig_noisy", σ)
+	
+	Pspec_noisy = heatmap(abs.(spec_harm_lin_noisy), title="Noisy", cbar=false);
+end
+
+# ╔═╡ 5092307e-6c23-46f9-b119-6ae8ce3e0604
+begin	
+	op_noisy=blasso.setSpecOperator(kernel, vec(spec_harm_lin_noisy));
+	lambda_noisy=0.01;
+	# Load objective function
+	fobj_noisy=blasso.setfobj(op_noisy,lambda_noisy);
+end
+
+# ╔═╡ 83877111-4e59-4c6d-830a-ecf9aea28bda
+result_noisy=sfw.sfw4blasso(fobj_noisy,kernel,op_noisy,options) # Solve problem
+
+# ╔═╡ 26fcfbb7-a611-4a0d-a6ed-b303e9ffad4c
+begin
+	println("Original parameters = $x0")
+	sfw.show_result(result, options)
+end
+
+# ╔═╡ 2f47da1b-b768-4f51-b83f-e6f67a231e2b
+md"## Interfering Case"
+
+# ╔═╡ dc8263c3-8e41-4918-bf70-f69d265b6597
+begin
+	py"""
+	from py_lib.signals import lin_chirp
+	from py_lib.spectrogram import gauss_spectrogram
+	
+	import numpy as np
+	#
+	# Two linear chirps
+	freq1 = 150
+	df = 60
+	N = 256
+	sig1,_ = lin_chirp(freq1, N=N)
+	sig2, t = lin_chirp(freq1 -df, N=N)
+	sig = sig1+sig2
+	
+	# adding noise
+	#sigma_noise = 0.2
+	#rng = np.random.default_rng()
+	#noise = rng.normal(0, sigma_noise, N) + rng.normal(0, sigma_noise, N)*1j
+	#sig_noisy = sig + noise
+	"""
+	# computing spectrogram
+	spec_harm_lin_interf = py"gauss_spectrogram"(py"sig", σ)
+	x0_interf = [[py"freq1/N",0], [py"(freq1-df)/N", 0]]
+	
+	Pspec_interf = heatmap(abs.(spec_harm_lin_interf), title="Interferences", cbar=false);
+end
+
+# ╔═╡ 4cc6a77e-6356-4557-b396-5c2ffd22b78e
+begin	
+	op_interf=blasso.setSpecOperator(kernel, vec(spec_harm_lin_interf));
+	lambda_interf=0.01;
+	# Load objective function
+	fobj_interf=blasso.setfobj(op_interf,lambda_interf);
+end
+
+# ╔═╡ bffd0638-e81d-4904-b7b6-67210dc0721b
+result_interf=sfw.sfw4blasso(fobj_interf,kernel,op_interf,options) # Solve problem
+
+# ╔═╡ a57b29f7-f3bf-4d50-bcd7-27e414df9822
+begin
+	println("Original parameters = $x0_interf")
+	sfw.show_result(result_interf, options)
+end
+
+# ╔═╡ 34a3bc8e-b75e-4797-9db3-db47711f34a0
+md"## Crossing case"
+
+# ╔═╡ caad4e0a-3b67-4fd6-a6be-4e9045de4c73
+begin
+	py"""
+	from py_lib.signals import lin_chirp
+	from py_lib.spectrogram import gauss_spectrogram
+	
+	import numpy as np
+	#
+	# Two linear chirps
+	N = 256
+	df = 100
+	freq1 = N//2 + df
+	freq2 = N//2 - df
+	sig1,_ = lin_chirp(freq1, c=-2*df, N=N)
+	sig2, t = lin_chirp(freq2, c=2*df, N=N)
+	sig = sig1+sig2
+	
+	# adding noise
+	#sigma_noise = 0.2
+	#rng = np.random.default_rng()
+	#noise = rng.normal(0, sigma_noise, N) + rng.normal(0, sigma_noise, N)*1j
+	#sig_noisy = sig + noise
+	"""
+	# computing spectrogram
+	spec_harm_lin_crossing = py"gauss_spectrogram"(py"sig", σ)
+	x0_crossing = [[py"freq1/N", -atan(py"2*df/N")], [py"freq2/N", atan(py"2*df/N")]]
+	
+	Pspec_crossing = heatmap(abs.(spec_harm_lin_crossing), title="Crossing Modes", cbar=false);
+end
+
+# ╔═╡ 9220d643-eda8-4464-ad2d-54c7ee7a82e3
+begin	
+	op_crossing=blasso.setSpecOperator(kernel, vec(spec_harm_lin_crossing));
+	lambda_crossing=0.01;
+	# Load objective function
+	fobj_crossing=blasso.setfobj(op_crossing,lambda_crossing);
+end
+
+# ╔═╡ 44184a53-9acc-449e-9fe3-0e3c23e796c5
+result_crossing=sfw.sfw4blasso(fobj_crossing,kernel,op_crossing,options) # Solve problem
+
+# ╔═╡ c877e2d4-3818-4316-aba0-c955e1af7fda
+begin
+	println("Original parameters = $x0_crossing")
+	sfw.show_result(result_crossing, options)
 end
 
 # ╔═╡ Cell order:
@@ -127,13 +253,27 @@ end
 # ╠═9e13dfd5-078d-49bb-827e-97575a6a42df
 # ╠═bb6f403c-0897-4903-be58-8cd320f83d17
 # ╟─5dcf6210-5e2d-4c74-854e-5617749d8b8c
+# ╟─648eff19-26fa-4d90-8968-daaacda95974
 # ╠═21f334a4-ef50-4e84-82c6-1d75a485d6b5
+# ╠═35e2ddf8-08da-40c9-9ec8-f6dc1e6b8d1c
+# ╟─81bdf362-fc97-499a-bb20-addabfe586ce
+# ╠═9c7084c4-e7cb-45db-a762-15b821ed7263
 # ╠═d71103a1-8e24-48b5-b6cd-9e9cf7a734a3
-# ╠═3d43a392-db29-4f4c-ba69-3d17d4978155
-# ╠═c468d81f-bfbc-4934-8518-58efcc551f72
 # ╠═436b02fb-2b8b-4e66-93ca-e344ecd90df0
-# ╠═67884e0d-db4a-4a6a-ace9-ec88efe65d14
 # ╠═01ed0bc2-3c35-4d51-8d31-bb084b592879
 # ╠═3c8fb520-419c-4626-b42c-38c813385179
-# ╠═959b2184-787b-465e-9d1c-178510d9ea21
-# ╠═9f87f847-e175-4029-8870-eeeba7b6cebd
+# ╟─0dec2a4e-dd48-4ad1-9f89-54d63877129b
+# ╠═7dea5cc0-9a54-4b55-90a5-9b1d3fc1fe5f
+# ╠═5092307e-6c23-46f9-b119-6ae8ce3e0604
+# ╠═83877111-4e59-4c6d-830a-ecf9aea28bda
+# ╠═26fcfbb7-a611-4a0d-a6ed-b303e9ffad4c
+# ╠═2f47da1b-b768-4f51-b83f-e6f67a231e2b
+# ╠═dc8263c3-8e41-4918-bf70-f69d265b6597
+# ╠═4cc6a77e-6356-4557-b396-5c2ffd22b78e
+# ╠═bffd0638-e81d-4904-b7b6-67210dc0721b
+# ╠═a57b29f7-f3bf-4d50-bcd7-27e414df9822
+# ╟─34a3bc8e-b75e-4797-9db3-db47711f34a0
+# ╠═caad4e0a-3b67-4fd6-a6be-4e9045de4c73
+# ╠═9220d643-eda8-4464-ad2d-54c7ee7a82e3
+# ╠═44184a53-9acc-449e-9fe3-0e3c23e796c5
+# ╠═c877e2d4-3818-4316-aba0-c955e1af7fda
