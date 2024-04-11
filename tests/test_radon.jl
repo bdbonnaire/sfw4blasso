d018d31c65143507c6dd4e4bef320429c217392d
Basile D. Bonnaire <bdbonnaires@gmail.com>
2024-04-11 10:09:44 +0200

    got radon to work for all lines

--- tests/test_radon.jl
+++ tests/test_radon.jl
@@ -14,6 +14,7 @@ using sfw
 # Model constants
 sigma=[1., 1.];
 M = 32.;
+MM = 65;
 px=range(-M, M);
 px=collect(px);
 py=px;
@@ -28,11 +29,11 @@ println(typeof(kernel))
 # %% Initial measure
 N = length(px)*length(py)
 a0 = [1., 1.]
-x0 = [[52., -pi/4], [-42., -pi/4]]
+x0 = [[53., -pi/4], [-42., -pi/4]]
 # Noise
 #srand(1);
 w0=randn(N);
-sigma_noise=0.0;
+sigma_noise=0.1;
 # Load operator Phi
 op=blasso.setGaussLineOperator(kernel,a0,x0,sigma_noise*w0);
 image = zeros((length(px),length(py)))
@@ -41,17 +42,28 @@ for i in 1:length(a0)
 end
 image += reshape(sigma_noise*w0, (length(px),length(py)))
 pIm = heatmap(image, aspect_ratio=1)
-
+plot!(x->M*cos(x)+M+1,x->M*sin(x)+M+1,0, 2pi; c=:black, lw=4)
 # %% Compute the Radon Transform
 angles = range(0, pi,200) |> collect
 
-radon_t = radon(image, angles);
+T = sqrt(2)M |> ceil
+T = convert(Int64, T)
+border_img = zeros((2T,2T))
+border_img[(2T-MM)÷2 .+ (1:MM), (2T-MM)÷2 .+ (1:MM)] = image
+heatmap(border_img)
+lines_radon = -temp:temp
+radon_t = radon(border_img, angles);
 pradon = heatmap(radon_t)
+# Computing peak of radon
+peak = argmax(vec(radon_t))
+peak_v = convert.(Float64, [peak % size(radon_t)[1], (peak ÷ size(radon_t)[1])+1])
+scatter!([peak_v[2]],[peak_v[1]])
 plot(pIm, pradon)
 
-peak = argmax(vec(radon_t))
-MM = 65
-peak_v = convert.(Float64, [peak % MM, (peak ÷ MM)+1])
-# radon_t[peak % MM, (peak ÷ MM)+1]
+
 peak_real = copy(peak_v)
 peak_real[2] *= -pi/200
+#=
+Discussion
+==========
+=#
