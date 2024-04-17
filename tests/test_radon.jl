1893c9603b8ab6352f1ad3d1a7ea2b79d28d3a39
Basile D. Bonnaire <bdbonnaires@gmail.com>
2024-04-17 15:59:35 +0200

    fixed radon -> line model method

--- tests/test_radon.jl
+++ tests/test_radon.jl
@@ -5,7 +5,6 @@ push!(LOAD_PATH,"../src")
 
 using Plots 
 using RadonKA
-import Peaks
 using blasso
 using sfw
 
@@ -28,12 +27,12 @@ println(typeof(kernel))
 #
 # %% Initial measure
 N = length(px)*length(py)
-a0 = [1., 1.]
-x0 = [[53., -pi/4], [-42., -pi/4]]
+a0=[60.]
+x0=[[15, -0.75]]
 # Noise
 #srand(1);
 w0=randn(N);
-sigma_noise=0.1;
+sigma_noise=0.031;
 # Load operator Phi
 op=blasso.setGaussLineOperator(kernel,a0,x0,sigma_noise*w0);
 image = zeros((length(px),length(py)))
@@ -43,6 +42,7 @@ end
 image += reshape(sigma_noise*w0, (length(px),length(py)))
 pIm = heatmap(image, aspect_ratio=1)
 plot!(x->M*cos(x)+M+1,x->M*sin(x)+M+1,0, 2pi; c=:black, lw=4)
+#
 # %% Compute the Radon Transform
 angles = range(0, pi,200) |> collect
 
@@ -51,7 +51,6 @@ T = convert(Int64, T)
 border_img = zeros((2T,2T))
 border_img[(2T-MM)÷2 .+ (1:MM), (2T-MM)÷2 .+ (1:MM)] = image
 heatmap(border_img)
-lines_radon = -temp:temp
 radon_t = radon(border_img, angles);
 pradon = heatmap(radon_t)
 # Computing peak of radon
@@ -62,8 +61,19 @@ plot(pIm, pradon)
 
 
 peak_real = copy(peak_v)
-peak_real[2] *= -pi/200
+peak_real[2] *= -pi/200;
+# peak_real[1] = abs(peak_real[1] - T)
+peak_real[1] -= T
+peak_real[1] /= cos(peak_real[2]);
+peak_real
+x0
+
 #=
 Discussion
 ==========
+The Radon Transform supposes a support in the unit disk. 
+In our case this meant that lines situated at the corners were not detected with the RT.
+To overcome this we added a border of √2M all around the image so that the inner disk of the new image
+encapsulates all of the information.
+With this the RT recovers all lines at the corners.
 =#
